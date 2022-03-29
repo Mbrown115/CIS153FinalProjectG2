178644bb11f1a5ea0c667615b804e31fd0765ef4
Mbrown115 <michaelbrian2001@gmail.com>
2022-03-28 22:33:52 -0400

    Constructing Initial Form
    Initial Commit

---
+++ CIS153_GitHubFinal/CIS153_GitHubFinal/Form1.cs
@@ -0,0 +1,27 @@
+﻿//-=----------------------=-
+//Authors: Michael Brown, 
+//Date:    3/28/2022
+//Desc:    Connect 4
+//-=----------------------=-
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CIS153_GitHubFinal
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
