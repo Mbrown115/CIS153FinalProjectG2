b2f42ca16cc469dad58226f03249f17668e1a199
Blake T <102618254+blaket93@users.noreply.github.com>
2022-04-05 21:17:54 -0400

    1play DBx Comment

--- CIS153_GitHubFinal/CIS153_GitHubFinal/Single.cs
+++ CIS153_GitHubFinal/CIS153_GitHubFinal/Single.cs
@@ -28,6 +28,13 @@ namespace CIS153_GitHubFinal
 
         private void btn_Exit_Click (object sender , EventArgs e)
         {
+            //*********************************************************************************************
+            //Working on a dialogbox to confirm "Exit" option
+            //Works if you only make a selection once, but if the user selects "No" after clicking exit
+            //and then  proceeds to click "Exit" again and "No" 2+ times before clicking "Yes"
+            // there is a weird unhandled exception. Tryin to reslove.
+            //*********************************************************************************************
+            
             //DialogResult result = MessageBox.Show ("Confirm Exit. Clicking Yes will end the game." , "Exiting Game!" , MessageBoxButtons.YesNo , MessageBoxIcon.Exclamation);
 
             //if (result == DialogResult.Yes)
