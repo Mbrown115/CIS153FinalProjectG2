dedfbec0afa478a663e856a9043a04696dbcc090
Blake T <102618254+blaket93@users.noreply.github.com>
2022-04-09 16:16:11 -0400

    Revert "game board form, turn label"
    This reverts commit 02e7051aaff200b1e5c7b825025bd649ea1fafd5.

--- CIS153_GitHubFinal/CIS153_GitHubFinal/NewPlayer.cs
+++ CIS153_GitHubFinal/CIS153_GitHubFinal/NewPlayer.cs
@@ -21,9 +21,7 @@ namespace CIS153_GitHubFinal
         {
             //Here is where the game board should be loaded
             //Names should save to STATS file to display individual player Stats vs. computer
-            //AND display TOTAL Player Wins vs TOTAL Computer wins. 
-                //If the player has chosen to play against the computer, Player 2 TextBox Should 
-                //NOT be active and text should read "Computer"
+            //AND display TOTAL Player Wins vs TOTAL Computer wins.
         }
     }
 }
