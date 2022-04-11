d141fb7df09fa295c96b9bd9d17ac5607e231a1f
Blake T <102618254+blaket93@users.noreply.github.com>
2022-04-11 19:29:41 -0400

    x

--- CIS153_GitHubFinal/CIS153_GitHubFinal/Single.cs
+++ CIS153_GitHubFinal/CIS153_GitHubFinal/Single.cs
@@ -30,12 +30,6 @@ namespace CIS153_GitHubFinal
 
         private void btn_Exit_Click(object sender, EventArgs e)
         {
-            //*********************************************************************************************
-            //Working on a dialogbox to confirm "Exit" option
-            //Works the first time, but if you run the program multiple times it gives you a weird
-            //unhandled exception when trying to terminate. Websearches haven't been helpful so far,
-            //working to resolve. 4/5/22 - BT
-            //*********************************************************************************************
 
             DialogResult result = MessageBox.Show("Confirm Exit. Clicking Yes will end the game.", "Exiting Game!", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
 
