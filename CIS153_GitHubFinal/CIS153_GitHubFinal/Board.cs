d4ea1229a34f7e425a25df4b5c26d1da00a75112
Quinn1232 <quintin.c.low@gmail.com>
2022-04-17 18:00:11 -0400

    Moved board, line, cell classes to diffrent class files

--- CIS153_GitHubFinal/CIS153_GitHubFinal/Board.cs
+++ CIS153_GitHubFinal/CIS153_GitHubFinal/Board.cs
@@ -2,46 +2,515 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace CIS153_GitHubFinal
 {
-    public class Board
+    class board
     {
-        // Setting Variables
-        private Rectangle[] columns;
-        private int[,] board;
+        private bool winner;
+        private int columns;
+        private int rows;
+        private string[,] grid;
+        private Button[,] buttons;
+        private float height;
+        private float width;
+        private float diag;
+        private cell last_token;
+        private string current_player;
+        private List<line> lines = new List<line>();
+        private List<line> playable = new List<line>();
+        private List<cell> streak_points = new List<cell>();
+        private List<cell> book_ends = new List<cell>(); //AI
+        private line streak_line; //AI
+        private List<cell> possible_plays = new List<cell>(); //AI
 
-        // Constructor
-        public Board()
+
+
+        public board(int c, int r, int s)
         {
+            this.grid = new string[r, c];
+            this.buttons = new Button[r, c];
+            for (int y = 0; y < this.grid.GetLength(0); y += 1)
+            {
+                for (int x = 0; x < this.grid.GetLength(1); x += 1)
+                {
+                    this.grid[y, x] = "-";
+                }
+            }
+            this.rows = this.grid.GetLength(0);
+            this.columns = this.grid.GetLength(1);
 
+            get_playable_dimensions(s);
+            this.find_lines();
+            this.find_playable();
+            this.points_on_line();
+            this.current_player = "x";
+            this.winner = false;
+        }
+        public void bot_AI()
+        {
+            bool dropped = false;
+            if (this.streak_of(3) == true)
+            {
+                foreach (cell C in this.book_ends)
+                {
+                    if (this.is_possible_move(C) == true)
+                    {
+                        //dropped = drop_token(C.get_column());
+                        Console.WriteLine("I would drop the token in this column " + C.get_column());
+                        if (dropped == true)
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+            if (dropped == false)
+            {
+                //drop_token(this.get_random_available_column());
+            }
+        }
+        public int get_random_availabe_column() //AI
+        {
+            int stack = -1;
+            Random rand = new Random();
+            List<int> available_stacks = new List<int>();
+            for (int c = 0; c < this.get_columns(); c++)
+            {
+                if (this.get_token(0, c) == "-")
+                {
+                    available_stacks.Add(c);
+                }
+            }
+            stack = available_stacks[rand.Next(0, available_stacks.Count)];
+            return (stack);
+        }
+        public bool is_possible_move(cell C) //AI
+        {
+            bool status = false;
+            foreach (cell P in this.possible_plays)
+            {
+                if (C.same(P) == true)
+                {
+                    status = true;
+                    break;
+                }
+            }
+            return (status);
+        }
+        public cell get_last_token()
+        {
+            return (this.last_token);
+        }
+        public void set_last_token(int row, int col)
+        {
+            this.last_token = new cell(row, col);
+        }
+        public bool get_winner()
+        {
+            return (this.winner);
+        }
+        public void set_winner(bool win)
+        {
+            this.winner = win;
+        }
+        public string get_player()
+        {
+            return (this.current_player);
+        }
+        public void set_player(string player)
+        {
+            this.current_player = player;
+        }
+        public int get_rows()
+        {
+            return (this.rows);
         }
 
-        //-=-----setter-------=-
-        public void setcolumns(Rectangle[] r)
+        public int get_columns()
         {
-            columns = r;
+            return (this.columns);
+        }
+        public Button get_button(int row, int col)
+        {
+            Button button = this.buttons[row, col];
+            return (button);
+        }
+        public void set_button(int row, int col, Button button)
+        {
+            this.buttons[row, col] = button;
+        }
+        public string get_token(int row, int col)
+        {
+            string token = this.grid[row, col];
+            return (token);
+        }
+        public void set_token(int row, int col, string token)
+        {
+            this.grid[row, col] = token;
+        }
+        public bool is_full(int col)
+        {
+            bool full = false;
+            if(this.get_token(0, col) != "-")
+            {
+                full = true;
+            }
+            return (full);
+        }
+        public bool hover_token(int col)
+        {
+            Button button;
+            bool status = false;
+            string token;
+            for (int row = this.get_rows() - 1; row >= 0; row--)
+            {
+                token = get_token(row, col);
+                if (token == "-")
+                {
+                    button = this.get_button(row, col);
+                    if (this.get_player() == "x")
+                    {
+                        button.BackColor = Color.Red;
+                    }
+                    else if (this.get_player() == "o")
+                    {
+                        button.BackColor = Color.Black;
+                    }
+                    this.set_last_token(row, col);
+                    status = true;
+                    break;
+                }
+            }
+            return (status);
+        }
+        public void figure_possible_plays() //AI
+        {
+            this.possible_plays.Clear();
+            string token;
+            cell invalid_cell = new cell(-1, -1);
+            for (int col = 0; col < this.get_columns(); col++)
+            {
+                if (this.get_token(0, col) == "-")
+                {
+                    for (int row = this.get_rows() - 1; row >= 0; row--)
+                    {
+                        token = get_token(row, col);
+                        if (token == "-")
+                        {
+                            cell C = new cell(row, col);
+                            this.possible_plays.Add(C);
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    this.possible_plays.Add(invalid_cell);
+                }
+            }
+        }
+        public void bot_play()
+        {
+            this.figure_possible_plays();
+            this.display_possible_moves();
+            if(this.streak_of(3) == true)
+            {
+                this.find_next_cells_of_streak();
+                this.display_book_ends();
+                this.bot_AI();
+                //this.display_streak_points();
+            }
+
+        }
+        public void display_possible_moves()
+        {
+            Console.WriteLine("possible moves");
+            foreach (cell c in this.possible_plays)
+            {
+                c.display();
+            }
+        }
+        public void display_book_ends()
+        {
+            Console.WriteLine("book ends");
+            foreach (cell c in this.book_ends)
+            {
+                c.display();
+            }
+        }
+        public void display_streak_points()
+        {
+            Console.WriteLine("streak points");
+            foreach (cell c in this.streak_points)
+            {
+                c.display();
+            }
+        }
+        public bool drop_token(int col)
+        {
+            bool status = false;
+            string token;
+            for(int row = this.get_rows() -1; row >= 0;row--)
+            {
+                token = get_token(row, col);
+                if (token == "-")
+                {
+                    this.set_token(row, col, this.current_player);
+                    status = true;
+                    break;
+                }
+            }
+            return (status);
+        }
+        public void find_next_cells_of_streak() //AI
+        {
+            //this.display_streak_points();
+            //this.streak_line.display();
+            this.book_ends.Clear();
+            line S = this.streak_line;
+            bool after_streak = false;
+            cell fsc = this.streak_points[0];
+            cell lsc = this.streak_points[this.streak_points.Count - 1];
+            cell prev_cell = new cell(-1, -1);
+            foreach (cell C in S.get_points_on_line())
+            {
+                C.display();
+                if ((C.same(fsc)) || (C.same(lsc)))
+                {
+                    if (this.book_ends.Count == 0)
+                    {
+                        book_ends.Add(prev_cell);
+                    }
+                    else
+                    {
+                        after_streak = true;
+                    }
+                }
+                else if (after_streak == true)
+                {
+                    this.book_ends.Add(C);
+                    break;
+                }
+                prev_cell = C;
+            }
         }
 
-        public void setBoard(int[,] b)
+        public bool streak_of(int streak_size)
         {
-            board = b;
+            bool streak = false;
+            string empty = "-";
+            string streak_token = "";
+            int current_streak = 0;
+            string current_element = "";
+            List<cell> line_streak_points = new List<cell>();
+            foreach (line L in this.playable)
+            {
+                line_streak_points.Clear();
+                streak_token = " ";
+                current_streak = 0;
+                foreach (cell P in L.get_points_on_line())
+                {
+                    try
+                    {
+                        current_element = grid[P.get_row(), P.get_column()];
+                    }
+#pragma warning disable 0168
+                    catch (Exception e)
+#pragma warning restore 0168
+                    {
+                        current_element = "-";
+                        Console.WriteLine("");
+                        Console.WriteLine("HuH!?!");
+                        P.display();
+                        Console.WriteLine("");
+                        Thread.Sleep(20);
+                    }
+                    if (current_element == empty)
+                    {
+                        streak_token = empty;
+                        current_streak = 0;
+                        if (streak == true)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            line_streak_points.Clear();
+                        }
+                    }
+                    else if (current_element != streak_token)
+                    {
+                        streak_token = current_element;
+                        current_streak = 1;
+                        if (streak == true)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            line_streak_points.Add(P);
+                        }
+                    }
+                    else if (current_element == streak_token)
+                    {
+                        current_streak++;
+                        line_streak_points.Add(P);
+                    }
+                    if (current_streak == streak_size)
+                    {
+                        streak = true;
+                        this.streak_line = L; //AI
+                    }
+                }
+                if (streak == true)
+                {
+                    this.streak_points = line_streak_points;
+                    //this.display_streak_points();
+                    break; //this may break the program
+                }
+                else
+                {
+                    //his.streak_points.Clear();
+                }
+            }
+            return (streak);
         }
-        //-=-----end setter-------=-
 
-        //-=-----getter-------=-
-        public Rectangle[] getcolumns()
+        public void points_on_line()
         {
-            return columns;
+            for (int row_index = 0; row_index < this.grid.GetLength(0); row_index++)
+            {
+                for (int col_index = 0; col_index < this.grid.GetLength(1); col_index++)
+                {
+                    cell test = new cell(row_index, col_index);
+                    foreach (line L in this.playable)
+                    {
+                        if (L.point_on_line(test))
+                        {
+                            L.add_point(test);
+                        }
+                    }
+                }
+            }
         }
 
-        public int[,] getboard()
+        public void get_playable_dimensions(int streak_size)
         {
-            return board;
+            this.height = this.grid.GetLength(0) - 1;
+            this.width = this.grid.GetLength(1) - 1;
+            this.diag = (float)Math.Sqrt(Math.Pow((streak_size - 1), 2) + Math.Pow((streak_size - 1), 2));
+            Console.WriteLine("height: {0}, width: {1}, diag: {2}", this.height, this.width, this.diag);
+            if ((this.height < streak_size) || (this.width < streak_size) || (this.diag < streak_size))
+            {
+                Console.WriteLine("board too small for streak of {0}", streak_size);
+                while (true == true)
+                {
+                    // infinite loop to hold program
+                }
+            }
         }
-        //-=-----end getter-------=-
+
+        public void find_playable()
+        {
+            foreach (line L in this.lines)
+            {
+                if ((L.get_no_slope() == true) || (L.get_slope() == 0.0) || (L.get_slope() == 1.0) || (L.get_slope() == -1.0))
+                {
+                    if (((L.get_no_slope() == true) && (L.get_length() == height)) || ((L.get_slope() == 0.0) && (L.get_length() == width)) || (((L.get_slope() == 1.0) || L.get_slope() == -1.0) && (L.get_length() >= diag)))
+                    {
+                        this.playable.Add(L);
+                    }
+                }
+            }
+        }
+
+        public void find_lines()
+        {
+            for (int a_row_index = 0; a_row_index < this.grid.GetLength(0); a_row_index++)
+            {
+                for (int a_col_index = 0; a_col_index < this.grid.GetLength(1); a_col_index++)
+                {
+                    cell a = new cell(a_row_index, a_col_index);
+                    if (is_point_on_perimeter(a))
+                    {
+                        for (int b_row_index = 0; b_row_index < this.grid.GetLength(0); b_row_index++)
+                        {
+                            for (int b_col_index = 0; b_col_index < this.grid.GetLength(1); b_col_index++)
+                            {
+                                cell b = new cell(b_row_index, b_col_index);
+                                if (is_point_on_perimeter(b))
+                                {
+                                    if (a.same(b))
+                                    {
+                                        continue;
+                                    }
+                                    line current = new line(a, b);
+                                    line reverse = new line(b, a);
+                                    if (line_exists(reverse))
+                                    {
+                                        // skip
+                                    }
+                                    else
+                                    {
+                                        this.lines.Add(current);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public bool line_exists(line l)
+        {
+            bool status = false;
+            foreach (line L in this.lines)
+            {
+                if (L.same(l))
+                {
+                    status = true;
+                    break;
+                }
+            }
+            return (status);
+        }
+
+        public bool is_point_on_perimeter(cell a)
+        {
+            bool status = false;
+            int x = (int)a.get_x();
+            int y = (int)a.get_y();
+            status = is_point_on_perimeter(y, x);
+            return (status);
+        }
+
+        public bool is_point_on_perimeter(int y, int x)
+        {
+            bool status = false;
+            int height = this.grid.GetLength(0) - 1;
+            int width = this.grid.GetLength(1) - 1;
+            int[] h_range = { 0, height };
+            int[] w_range = { 0, width };
+            if (h_range.Contains(Math.Abs(y)) || w_range.Contains(Math.Abs(x)))
+            {
+                status = true;
+            }
+            return (status);
+        }
+        public void display()
+        {
+            for (int y = 0; y < this.grid.GetLength(0); y++)
+            {
+                for (int x = 0; x < this.grid.GetLength(1); x++)
+                {
+                    Console.Write(this.grid[y, x]);
+                }
+                Console.WriteLine();
+            }
+        }
     }
 }
---
+++ CIS153_GitHubFinal/CIS153_GitHubFinal/Cell_old.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CIS153_GitHubFinal
+{
+    public class Cell_old
+    {
+        // Setting variables
+        private int neutral;
+        private int player1;
+        private int player2;
+
+        // Constructor
+        public Cell_old()
+        {
+
+        }
+
+        //-=--------setters----------=-
+        public void setNeutral(int n)
+        {
+            neutral = n;
+        }
+        public void setPlayer1(int p1)
+        {
+            player1 = p1;
+        }
+        public void setPlayer2(int p2)
+        {
+            player2 = p2;
+        }
+        //-=--------end setters----------=-
+
+        //-=--------getters----------=-
+        public int getNeutral()
+        {
+            return neutral;
+        }
+        public int getPlayer1()
+        {
+            return player1;
+        }
+        public int getPlayer2()
+        {
+            return player2;
+        }
+        //-=--------end getters----------=-
+    }
+}
