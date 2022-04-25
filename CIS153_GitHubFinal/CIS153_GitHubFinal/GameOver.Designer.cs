f43a3d04f220c47361b5fc6e8cbfeb119d245aef
Remi <rethun@my.monroeccc.edu>
2022-04-24 22:01:56 -0400

    game over stats

--- CIS153_GitHubFinal/CIS153_GitHubFinal/GameOver.Designer.cs
+++ CIS153_GitHubFinal/CIS153_GitHubFinal/GameOver.Designer.cs
@@ -34,7 +34,10 @@ namespace CIS153_GitHubFinal
             this.lbl_NewGame = new System.Windows.Forms.Label();
             this.picBx_WinLose = new System.Windows.Forms.PictureBox();
             this.btn_Review = new System.Windows.Forms.Button();
-            this.lbl_gameOverStats = new System.Windows.Forms.Label();
+            this.lbl_pxWins = new System.Windows.Forms.Label();
+            this.lbl_poWins = new System.Windows.Forms.Label();
+            this.lbl_pxStats = new System.Windows.Forms.Label();
+            this.lbl_poStats = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.picBx_WinLose)).BeginInit();
             this.SuspendLayout();
             // 
@@ -103,15 +106,49 @@ namespace CIS153_GitHubFinal
             this.btn_Review.UseVisualStyleBackColor = false;
             this.btn_Review.Click += new System.EventHandler(this.btn_Review_Click);
             // 
-            // lbl_gameOverStats
+            // lbl_pxWins
             // 
-            this.lbl_gameOverStats.AutoSize = true;
-            this.lbl_gameOverStats.BackColor = System.Drawing.Color.Transparent;
-            this.lbl_gameOverStats.Location = new System.Drawing.Point(458, 297);
-            this.lbl_gameOverStats.Name = "lbl_gameOverStats";
-            this.lbl_gameOverStats.Size = new System.Drawing.Size(35, 13);
-            this.lbl_gameOverStats.TabIndex = 14;
-            this.lbl_gameOverStats.Text = "label1";
+            this.lbl_pxWins.AutoSize = true;
+            this.lbl_pxWins.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_pxWins.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_pxWins.Location = new System.Drawing.Point(321, 295);
+            this.lbl_pxWins.Name = "lbl_pxWins";
+            this.lbl_pxWins.Size = new System.Drawing.Size(400, 48);
+            this.lbl_pxWins.TabIndex = 14;
+            this.lbl_pxWins.Text = "Player X Total Wins:";
+            // 
+            // lbl_poWins
+            // 
+            this.lbl_poWins.AutoSize = true;
+            this.lbl_poWins.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_poWins.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_poWins.Location = new System.Drawing.Point(321, 343);
+            this.lbl_poWins.Name = "lbl_poWins";
+            this.lbl_poWins.Size = new System.Drawing.Size(405, 48);
+            this.lbl_poWins.TabIndex = 15;
+            this.lbl_poWins.Text = "Player O Total Wins:";
+            // 
+            // lbl_pxStats
+            // 
+            this.lbl_pxStats.AutoSize = true;
+            this.lbl_pxStats.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_pxStats.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_pxStats.Location = new System.Drawing.Point(629, 295);
+            this.lbl_pxStats.Name = "lbl_pxStats";
+            this.lbl_pxStats.Size = new System.Drawing.Size(134, 48);
+            this.lbl_pxStats.TabIndex = 16;
+            this.lbl_pxStats.Text = "empty";
+            // 
+            // lbl_poStats
+            // 
+            this.lbl_poStats.AutoSize = true;
+            this.lbl_poStats.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_poStats.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_poStats.Location = new System.Drawing.Point(631, 341);
+            this.lbl_poStats.Name = "lbl_poStats";
+            this.lbl_poStats.Size = new System.Drawing.Size(134, 48);
+            this.lbl_poStats.TabIndex = 17;
+            this.lbl_poStats.Text = "empty";
             // 
             // GameOver
             // 
@@ -120,7 +157,10 @@ namespace CIS153_GitHubFinal
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(984, 960);
             this.ControlBox = false;
-            this.Controls.Add(this.lbl_gameOverStats);
+            this.Controls.Add(this.lbl_poStats);
+            this.Controls.Add(this.lbl_pxStats);
+            this.Controls.Add(this.lbl_poWins);
+            this.Controls.Add(this.lbl_pxWins);
             this.Controls.Add(this.btn_Review);
             this.Controls.Add(this.btn_NewGame);
             this.Controls.Add(this.btn_Exit);
@@ -142,6 +182,9 @@ namespace CIS153_GitHubFinal
         private System.Windows.Forms.Label lbl_NewGame;
         private System.Windows.Forms.PictureBox picBx_WinLose;
         private System.Windows.Forms.Button btn_Review;
-        private System.Windows.Forms.Label lbl_gameOverStats;
+        private System.Windows.Forms.Label lbl_pxWins;
+        private System.Windows.Forms.Label lbl_poWins;
+        private System.Windows.Forms.Label lbl_pxStats;
+        private System.Windows.Forms.Label lbl_poStats;
     }
 }
--- CIS153_GitHubFinal/CIS153_GitHubFinal/GameOver.cs
+++ CIS153_GitHubFinal/CIS153_GitHubFinal/GameOver.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace CIS153_GitHubFinal
 {
@@ -38,6 +39,36 @@ namespace CIS153_GitHubFinal
             {
                 picBx_WinLose.Image = Properties.Resources.Draw_png;
             }
+
+            readFile();
+        }
+
+        public void readFile()
+        {
+            string filePath = "Records.txt";
+            string pxWins = "";
+            string poWins = "";
+            string ties = "";
+            string games = "";
+
+            StreamReader statsRead = new StreamReader(filePath);
+            string line = statsRead.ReadLine();
+
+            while (line != null)
+            {
+                String[] words = line.Split(',');
+
+                pxWins = words[0];
+                poWins = words[1];
+                ties = words[2];
+                games = words[3];
+
+                line = statsRead.ReadLine();
+            }
+            statsRead.Close();
+
+            lbl_pxStats.Text = pxWins;
+            lbl_poStats.Text = poWins;
         }
 
 
